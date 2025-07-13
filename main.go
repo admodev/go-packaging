1779f8f21d3ba3ada6f327ae0199b218efa2486f
admodev <admodevcodes@outlook.com>
2025-07-13 13:50:56 -0300

    feat: :sparkles: add internal and pkg folder creation

--- main.go
+++ main.go
@@ -22,9 +22,25 @@ func main() {
 	fmt.Println("⏱️ Creating binary folders...")
 
 	binaryPath := fmt.Sprintf("./cmd/%s/", appName)
-	mainFile := fmt.Sprintf("%s/main.go", binaryPath)
+	entryPoint := fmt.Sprintf("%s/main.go", binaryPath)
 
 	os.MkdirAll(binaryPath, os.ModePerm)
-	os.OpenFile(mainFile, os.O_RDONLY|os.O_CREATE, 0666)
+	os.OpenFile(entryPoint, os.O_RDONLY|os.O_CREATE, 0666)
 	fmt.Println("✔️ Binary folders created successfully!")
+
+	// Functional domain packages
+	fmt.Println("⏱️ Creating internal folders...")
+	os.MkdirAll("./internal/rename_me/", os.ModePerm)
+	os.MkdirAll("./internal/config/", os.ModePerm)
+	os.OpenFile("./internal/rename_me/example_file.go", os.O_RDONLY|os.O_CREATE, 0666)
+	os.OpenFile("./internal/rename_me/example_file_test.go", os.O_RDONLY|os.O_CREATE, 0666)
+	os.OpenFile("./internal/config/config.go", os.O_RDONLY|os.O_CREATE, 0666)
+	os.OpenFile("./internal/config/config_test.go", os.O_RDONLY|os.O_CREATE, 0666)
+	fmt.Println("✔️ Internal folders created successfully!")
+
+	// Reusable exported code
+	fmt.Println("⏱️ Creating pkg folders...")
+	os.MkdirAll("./pkg/example_package/", os.ModePerm)
+	os.OpenFile("./internal/example_package/example_package_file.go", os.O_RDONLY|os.O_CREATE, 0666)
+	fmt.Println("✔️ Pkg folders created successfully!")
 }
